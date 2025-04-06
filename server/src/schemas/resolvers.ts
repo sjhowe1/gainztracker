52470e1f2a79da0129d53eddaedcc09bcfd75c9b
Monique Quinones <quinones9980@gmail.com>
2025-04-06 18:35:02 -0500

    Interfaces & Querys

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -4,7 +4,7 @@ import { signToken, AuthenticationError } from "../utils/auth";
 interface User {
     _id: string;
     username: string;
-    email: string;
+    mail: string;
     password: string;
     workouts: string[];
     firstName: string;
@@ -25,10 +25,45 @@ interface AddUserArgs {
         firstName: string;
         lastName: string;
     }
-}  
+}
 
-const resolvers = {
+interface AddWorkoutArgs {
+     userId: string;
+     workout: string;
+    }
 
-};
+interface RemoveWorkoutArgs {
+    userId: string;
+    workout: string;
+}
 
+interface AddExerciseArgs {
+    userId: string;
+    workout: string;
+    exercise: string;
+}
+
+interface RemoveExerciseArgs {
+    userId: string;
+    workout: string;
+    exercise: string;
+}
+
+interface Context {
+    user?: User;
+}
+
+const Resolvers = {
+  Query: {
+      users: async (): Promise<User[]> => {
+          return await User.find();
+      },
+      user: async (_parent: any, { userId }: UserArgs): Promise<User | null> => {
+          return await User.findOne ({ _id: userId });
+      },
+      workouts: async (): Promise<Workout[]> => {
+            return await Workout.find();
+        }
+               
+        
 export default resolvers;
