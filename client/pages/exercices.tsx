7be322a73ef3658ca785d590fc8db88bac6e3aca
Lance Anderson <lanceanderson2005@gmail.com>
2025-04-07 20:30:35 -0500

    adding to my branch to switch

---
+++ client/pages/exercices.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Exercices: React.FC = () => {
+    return (
+        <div>
+            <h1>Exercises</h1>
+            <p>Welcome to the exercises page!</p>
+        </div>
+    );
+};
+
+export default Exercices;
