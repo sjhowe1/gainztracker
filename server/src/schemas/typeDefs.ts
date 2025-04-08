85d01ae0e0e3763615d956bc0adb5bf56583b91a
Sherard Howe <mr.s.j.howe1@icloud.com>
2025-04-07 20:06:11 -0500

    chore: Added types to typeDefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -14,6 +14,42 @@ const typeDefs = `
         user: User
     }
 
+    type Muscle {
+        _id: ID
+        name: String
+    }
+
+    type Workout {
+        _id: ID
+        description: String
+        primaryMuscle: Muscle
+        secondaryMuscles: [Muscle];
+        exercises: [Exercise]
+    }
+
+    type Set {
+        _id: ID
+        number: Int
+        reps: Int
+    }
+
+    type Exercise {
+        _id: ID
+        name: String
+        description: String
+        primaryMuscle: Muscle
+        secondaryMuscles: [Muscle]
+        video: Video
+        sets: [Set]
+    }
+
+    type Video {
+        _id: ID
+        name: String
+        description: String
+        url: String
+    }
+
     input UserInput {
         firstName: String!
         lastName: String!
--- server/src/types/exercise.ts
+++ server/src/types/exercise.ts
@@ -3,6 +3,7 @@ import { Document } from "mongoose";
 
 export interface IExercise extends Document {
     _id: string;
+    name: string;
     description: string;
     primaryMuscle: Muscle;
     secondaryMuscles: Array<Muscle>;
