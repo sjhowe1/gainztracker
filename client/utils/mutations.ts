62ed7d63c6a9161d537a1bc708f6fee7f4e66f29
Sherard Howe <mr.s.j.howe1@icloud.com>
2025-04-09 22:46:55 -0500

    chore: Completed client side graphQL Mutations

--- client/utils/mutations.ts
+++ client/utils/mutations.ts
@@ -17,6 +17,7 @@ export const LOGIN_USER = gql`
     }
 `;
 
+//Add Workout Mutation
 export const ADD_WORKOUT = gql`
   mutation addWorkout($userId: ID!, $name: String!, $description: String!, $primaryMuscle: String!, $secondaryMuscles: [String]) {
     addWorkout(userId: $userId, name: $name, description: $description, primaryMuscle: $primaryMuscle, secondaryMuscles: $secondaryMuscles) {
@@ -27,6 +28,7 @@ export const ADD_WORKOUT = gql`
   }
 `;
 
+//Add Exercise Mutation
 export const ADD_EXERCISE = gql`
   mutation addExercise($workoutId: ID!, $name: String!, $description: String!, $primaryMuscle: String!, $secondaryMuscles: [String]) {
     addExercise(workoutId: $workoutId, name: $name, description: $description, primaryMuscle: $primaryMuscle, secondaryMuscles: $secondaryMuscles) {
@@ -37,6 +39,7 @@ export const ADD_EXERCISE = gql`
   }
 `;
 
+//Add Set Mutation
 export const ADD_SET = gql`
   mutation addSet($exerciseId: ID!, $setNumber: Int!, $reps: Int!, $weight: Int!) {
     addSet(exerciseId: $exerciseId, setNumber: $setNumber, reps: $reps, weight: $weight) {
@@ -44,6 +47,39 @@ export const ADD_SET = gql`
       setNumber
       reps
       weight
+    }
+  }
+`;
+
+//Remove Workout Mutation
+export const REMOVE_WORKOUT = gql`
+  mutation removeWorkout($workoutId: ID!) {
+    removeWorkout(workoutId: $workoutId) {
+      _id
+      username
+      workouts
+    }
+  }
+`;
+
+//Remove Exercise Mutation
+export const REMOVE_EXERCISE = gql`
+  mutation removeExercise($workoutId: ID!, $exerciseName: String!) {
+    removeExercise(workoutId: $workoutId, exerciseName: $exerciseName) {
+      _id
+      name
+      exercises
+    }
+  }
+`;
+
+//Remove Set Mutation
+export const REMOVE_SET = gql`
+  mutation removeSet($exerciseId: ID!, $setNumber: Int!) {
+    removeSet(exerciseId: $exerciseId, setNumber: $setNumber) {
+      _id
+      name
+      sets
     }
   }
 `;
